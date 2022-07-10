b58e752f0ddfd3cfeb3d7266d3f82b4a36a416d5
stnblog <tanabesetsuto807@gmail.com>
2022-07-10 23:05:13 +0900

    update

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (entry.isIntersecting) {
           setTimeout(function () {
             entry.target.classList.add('is-anim');
-          }, 400);
+          }, 200);
         } else {}
       });
     };
